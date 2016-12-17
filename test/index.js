e50640ba6e02afcf1229469bead759997808eee9
Anthony Graddy <anthony@locationq.com>
2016-12-17 06:10:06 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,23 @@
 var tap = require('agraddy.test.tap')(__filename);
+var response = require('agraddy.test.res');
 
-var mod = require('../');
+var drop = require('../');
+var routes = {};
+var res = response();
+var data = {};
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+process.chdir('test');
+
+data.vault = {};
+data.vault.data = {};
+data.vault.config = {};
+data.vault.data.title = 'HBS';
+
+res.on('finish', function() {
+	tap.assert.equal(res._body, '-HBS-\n', 'Should replace the {{data.title}}.');
+});
+
+drop('views/about.htm')(null, {}, res, data);
+
 
 
